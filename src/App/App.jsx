c3991a0ca374e00109c60c1ba5a12e3955a49d15
smaurier <sylvain.maurier@gmail.com>
2022-02-18 11:57:44 +0100

    clean app.js

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { CharactersContextProvider } from '../contexts/characterContext';
-import { SearchResultContextProvider } from '../contexts/resultContext';
 import MainRouter from '../router';
 import './App.scss';
 
 function App() {
 	return (
 		<>
-			<SearchResultContextProvider>
-				<CharactersContextProvider>
-					<MainRouter />
-				</CharactersContextProvider>
-			</SearchResultContextProvider>
+			<CharactersContextProvider>
+				<MainRouter />
+			</CharactersContextProvider>
 		</>
 	);
 }
